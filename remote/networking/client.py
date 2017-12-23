c0de85580cf63ba7fc762a5ca2fdd57c779fdcc0
Amin Etesamian <aminetesamian1371@gmail.com>
2017-12-23 14:26:24 -0800

    tcpconnector context manager

--- remote/networking/client.py
+++ remote/networking/client.py
@@ -8,6 +8,12 @@ class TCPRemoteClient(object):
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP over IP
         self.is_alive = False
 
+    def __enter__(self):
+        self.connect()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.hang_up()
+
     def connect(self):
         if not self.is_alive:
             self.connection.connect((self.host, self.port))
